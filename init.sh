0ca7dcb9bdda6cae3a5990051488c0fd8b4948e0
lntvan1606 <70436051+lntvan166@users.noreply.github.com>
2022-07-29 17:31:48 +0700

    init.bash

---
+++ init.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/2tech-US/e2tech-api-gateway.git
+git clone https://github.com/2tech-US/e2tech-auth-svc.git
+git clone https://github.com/2tech-US/e2tech-passenger-svc.git
+git clone https://github.com/2tech-US/e2tech-booking-svc.git
