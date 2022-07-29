1439b018e1d313e1b93b7eb792f096c8f8b29e50
lntvan1606 <70436051+lntvan166@users.noreply.github.com>
2022-07-29 17:52:43 +0700

    fix bash

--- up.sh
+++ up.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 docker run --name e2tech -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -p 5432:5432 -d postgres:12-alpine
-
+sleep 2
 while :; do
     ! [ "$(docker ps -aq -f status=running -f name=e2tech)" ] || break
     echo waiting
 done
 
-cd e2tech-auth-svc && make createdb && make migrateup && cd ..
-cd 2tech-passenger-svc && make createdb && make migrateup && cd ..
-cd e2tech-booking-svc && make createdb && make migrateup && cd ..
+cd e2tech-auth-svc
+make createdb
+make migrateup
+cd ..
+cd e2tech-passenger-svc
+make createdb
+make migrateup
+cd ..
+cd e2tech-booking-svc
+make createdb
+make migrateup
+cd ..
