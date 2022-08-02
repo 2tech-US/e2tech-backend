b303cf9c9ba67cbf943d5f32e3ecbe4208fc2b7c
lntvan1606 <70436051+lntvan166@users.noreply.github.com>
2022-08-02 13:58:38 +0700

    start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd ..
 cd e2tech-auth-svc && make auth_svc &
-cd ..
 cd e2tech-passenger-svc && make passenger_svc &
-cd ..
 cd e2tech-booking-svc && make booking_svc &
-cd ..
 cd e2tech-api-gateway && make api_gateway
